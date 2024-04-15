5db7c3ac81baa60254eb321e8c048276a724503e
Mridul Pant <mridulpant2010@gmail.com>
2024-04-15 23:13:56 +0530

    created directory structure and test code

---
+++ huffman_encoding/src/main.py
@@ -0,0 +1,17 @@
+
+def read_file(file_path,file_name):
+    with open(file_path, 'r') as f:
+        data = f.readline()
+        # TODO: read each character and prepare a frequency map
+        '''my approach
+        - TODO[primary]: file_path default is the cwd, take file_name as input argument.
+        - TODO[optional]: otherwise the first argument is the file_path and the 
+        
+        split each of the data into a list and iterate over each of the character
+        
+        better approach
+        
+        
+        '''
+
+
