6a851666e90cfc29730f895fe6efb82270a1460a
Mridul Pant <mridulpant2010@gmail.com>
2024-04-24 23:23:33 +0530

    template code

--- huffman_encoding/src/main.py
+++ huffman_encoding/src/main.py
@@ -1,3 +1,5 @@
+import heapq
+
 
 def read_file(file_path,file_name):
     with open(file_path, 'r') as f:
@@ -13,5 +15,29 @@ def read_file(file_path,file_name):
         
         
         '''
+        
+def build_frequency_map():
+    pass
 
 
+def build_tree() -> None:
+    pass
+
+class Node:
+    def __init__(self,val,weight):
+        self.val=val
+        self.weight=weight
+
+def build_heap(frequency_map:dict) -> None:
+    he = []
+    for k,v in frequency_map.iteritems():
+        heapq.heappush(he,Node(k,v))
+
+    while len(he) > 0:
+        first= heapq.heappop(he)
+        second = heapq.heappop(he)
+        third = Node(first.val + second.val)
+        heapq.heappush(he,third)
+    print(third)
+        
+    
